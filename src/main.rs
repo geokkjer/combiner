786cfb310828bf201547e0c613c711c6ae0cc7dc
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2022-04-10 19:09:13 +0200

    wrote more code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,52 @@
 mod args;
 use args::Args;
+use image::{ io::Reader, DynamicImage, ImageFormat, imageops::FilterType::Triangle, GenericImageView};
+use std::{ io::BufReader, fs::File};
 
-fn main() {
-    let args = Args::new();
-    println!("{:?}", args);
+enum ImageDataErrors {
+    DifferentImageFormats,
 }
 
+struct FloatingImage {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+}
+
+
+fn main() -> Result<(), ImageDataErrors> {
+    let args = Args::new();
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+    
+    if image_format_1 != image_format_2 {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+
+    let (image_1, image_2) = standardise_size(image_1, image_2);
+    Ok(())
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format = image_reader.format().unwrap();
+    let image = image_reader.decode().unwrap();
+    (image, image_format)
+}
+
+fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pix_1 = dim_1.0 * dim_1.1;
+    let pix_2 = dim_2.0 * dim_2.1;
+    return if pix_1 < pix_2 { dim_1 } else { dim_2 };
+}
+
+fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (width, height) = get_smallest_dimension(image_1.dimensions(), image_2.dimensions());
+    println!("width: {}, height: {}\n", width, height);
+
+    if image_2.dimensions() == (width, height) {
+        (image_1.resize_exact(width, height, Triangle), image_2)
+    } else {
+        (image_1, image_2.resize_exact(width, height, Triangle))
+    }
+}
